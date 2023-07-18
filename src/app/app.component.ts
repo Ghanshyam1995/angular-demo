674329ba715915d87c51115312c44e317ac0f8ba
Ghanshyam Singh Garia <ghanshyamsingh.garia@jci.com>
2023-07-18 17:51:04 +0530

    commit add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demoangular';
+  title = 'Github actions showing error';
 }
